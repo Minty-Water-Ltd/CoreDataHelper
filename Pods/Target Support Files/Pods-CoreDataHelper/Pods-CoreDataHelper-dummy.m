54f8c39052024a4c631726d980321471d21b41e5
Chris Wunsch <chris.wunsch@conjure.co.uk>
2016-11-29 14:43:58 +0000

    Removed pods as none were being used

--- Pods/Target Support Files/Pods-CoreDataHelper/Pods-CoreDataHelper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoreDataHelper : NSObject
-@end
-@implementation PodsDummy_Pods_CoreDataHelper
-@end
--- Pods/Target Support Files/Pods-CoreDataHelperTests/Pods-CoreDataHelperTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_CoreDataHelperTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CoreDataHelperTestsVersionString[];
-
