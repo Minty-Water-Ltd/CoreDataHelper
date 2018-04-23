b227d3d481de0d34f51bce97122f0b84e03b0d8b
Chris Wunsch <chris@mintywater.co.uk>
2018-04-23 11:08:13 +0100

    Added example

---
+++ CoreDataHelper/CoreDataHelper.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataHelper.h
+//  CoreDataHelper
+//
+//  Created by Chris Wunsch on 23/04/2018.
+//  Copyright © 2018 Minty Water Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataHelper.
+FOUNDATION_EXPORT double CoreDataHelperVersionNumber;
+
+//! Project version string for CoreDataHelper.
+FOUNDATION_EXPORT const unsigned char CoreDataHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataHelper/PublicHeader.h>
+
+
